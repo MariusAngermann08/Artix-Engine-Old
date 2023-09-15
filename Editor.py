8a2594d725f600b74968812e3bc1ee7a538cfbd7
Marius Angermann <134206019+MariusAngermann08@users.noreply.github.com>
2023-09-15 18:14:01 +0200

    Screen Resolution Bug fixed
    If you have a low display resolution until now you werent able to see all of the program. Now when starting the editor it changes your screen resolution to 1920x1080 an donce you closes back to your default.

--- Editor.py
+++ Editor.py
@@ -29,6 +29,11 @@ import re
 from Export import Export
 
 
+
+
+
+
+
 openfile = open("prcopen.info", "r")
 readfile = openfile.readlines()
 project_name = readfile[0]
@@ -45,7 +50,139 @@ if sys.platform.startswith('win'):
 
 
 
+class ScreenRes(object):
+    @classmethod
+    def set(cls, width=None, height=None, depth=32):
+        '''
+        Set the primary display to the specified mode
+        '''
+        if width and height:
+            print('Setting resolution to {}x{}'.format(width, height, depth))
+        else:
+            print('Setting resolution to defaults')
 
+        if sys.platform == 'win32':
+            cls._win32_set(width, height, depth)
+        elif sys.platform.startswith('linux'):
+            cls._linux_set(width, height, depth)
+        elif sys.platform.startswith('darwin'):
+            cls._osx_set(width, height, depth)
+
+    @classmethod
+    def get(cls):
+        if sys.platform == 'win32':
+            return cls._win32_get()
+        elif sys.platform.startswith('linux'):
+            return cls._linux_get()
+        elif sys.platform.startswith('darwin'):
+            return cls._osx_get()
+
+    @classmethod
+    def get_modes(cls):
+        if sys.platform == 'win32':
+            return cls._win32_get_modes()
+        elif sys.platform.startswith('linux'):
+            return cls._linux_get_modes()
+        elif sys.platform.startswith('darwin'):
+            return cls._osx_get_modes()
+
+    @staticmethod
+    def _win32_get_modes():
+        '''
+        Get the primary windows display width and height
+        '''
+        import win32api
+        from pywintypes import DEVMODEType, error
+        modes = []
+        i = 0
+        try:
+            while True:
+                mode = win32api.EnumDisplaySettings(None, i)
+                modes.append((
+                    int(mode.PelsWidth),
+                    int(mode.PelsHeight),
+                    int(mode.BitsPerPel),
+                    ))
+                i += 1
+        except error:
+            pass
+
+        return modes
+
+    @staticmethod
+    def _win32_get():
+        '''
+        Get the primary windows display width and height
+        '''
+        import ctypes
+        user32 = ctypes.windll.user32
+        screensize = (
+            user32.GetSystemMetrics(0), 
+            user32.GetSystemMetrics(1),
+            )
+        return screensize
+
+    @staticmethod
+    def _win32_set(width=None, height=None, depth=32):
+        '''
+        Set the primary windows display to the specified mode
+        '''
+        # Gave up on ctypes, the struct is really complicated
+        #user32.ChangeDisplaySettingsW(None, 0)
+        import win32api
+        from pywintypes import DEVMODEType
+        if width and height:
+
+            if not depth:
+                depth = 32
+
+            mode = win32api.EnumDisplaySettings()
+            mode.PelsWidth = width
+            mode.PelsHeight = height
+            mode.BitsPerPel = depth
+
+            win32api.ChangeDisplaySettings(mode, 0)
+        else:
+            win32api.ChangeDisplaySettings(None, 0)
+
+
+    @staticmethod
+    def _win32_set_default():
+        '''
+        Reset the primary windows display to the default mode
+        '''
+        # Interesting since it doesn't depend on pywin32
+        import ctypes
+        user32 = ctypes.windll.user32
+        # set screen size
+        user32.ChangeDisplaySettingsW(None, 0)
+
+    @staticmethod
+    def _linux_set(width=None, height=None, depth=32):
+        raise NotImplementedError()
+
+    @staticmethod
+    def _linux_get():
+        raise NotImplementedError()
+
+    @staticmethod
+    def _linux_get_modes():
+        raise NotImplementedError()
+
+    @staticmethod
+    def _osx_set(width=None, height=None, depth=32):
+        raise NotImplementedError()
+
+    @staticmethod
+    def _osx_get():
+        raise NotImplementedError()
+
+    @staticmethod
+    def _osx_get_modes():
+        raise NotImplementedError()
+
+default_screen_res = ScreenRes.get()
+ScreenRes.set(1920, 1080)
 
 
 #default costumtkinter setup
@@ -1928,6 +2065,14 @@ properties_apply_button.place(x=170,y=5,relwidth=0.3)
 general_update("startup")
 scenetree.select(scenetree.displayed_objects[0])
 viewport.update()
+
+def on_closing():
+    if tk.messagebox.askokcancel("Quit", "Do you want to quit?"):
+        ScreenRes.set(default_screen_res[0],default_screen_res[1])
+        app.destroy()
+        sys.exit()
+
+app.wm_protocol("WM_DELETE_WINDOW", on_closing)
 
 app.mainloop()
 
